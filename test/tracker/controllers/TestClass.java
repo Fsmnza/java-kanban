8ea539c3a4253dca9fe736a07cdc40407a5ba646
Fsmnza <140049580+Fsmnza@users.noreply.github.com>
2025-03-28 23:16:47 +0500

    Добавление в branches

--- test/tracker/controllers/TestClass.java
+++ test/tracker/controllers/TestClass.java
@@ -10,6 +10,8 @@ import tracker.model.Task;
 import tracker.util.Managers;
 import tracker.util.Status;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestClass {
@@ -107,5 +109,30 @@ class TestClass {
         assertEquals(task1.getDescription(), taskId.getDescription());
         assertEquals(task1.getStatus(), taskId.getStatus());
 
+    }
+    @Test
+    void newTaskCheckingGetTasks() {
+        var get = new InMemoryHistoryManager();
+        Task task1 = new Task("Стать программистом", "Очень много работать", Status.IN_PROGRESS);
+        get.linkLast(task1);
+        List<Task> newGetTask = get.getTasks();
+        assertEquals(1, newGetTask.size());
+        assertEquals(task1, newGetTask.get(0));
+    }
+
+    @Test
+    void newTaskCheckingLinkLast() {
+        var link = new InMemoryHistoryManager();
+        Task task1 = new Task("Стать программистом", "Очень много работать", Status.IN_PROGRESS);
+        link.linkLast(task1);
+    }
+
+    @Test
+    void newTaskCheckingRemove() {
+        var removeTask = new InMemoryHistoryManager();
+        Task task1 = new Task("Стать программистом", "Очень много работать", Status.IN_PROGRESS);
+        removeTask.linkLast(task1);
+        removeTask.remove(0);
+        assertTrue(removeTask.getTasks().isEmpty());
     }
 }
